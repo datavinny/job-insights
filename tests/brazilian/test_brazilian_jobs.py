363ee7eef17d26536dc7f475312e2aacb51ee1c9
Davi <df828316@gmail.com>
2022-10-20 19:36:06 -0300

    test: :test_tube: test for the method brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,15 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
+
+
+PATH = "tests/mocks/brazilians_jobs.csv"
 
 
 def test_brazilian_jobs():
-    pass
+    results = read_brazilian_file(PATH)
+    assert type(results) == list
+    for result in results:
+        assert type(result) == dict
+
+    assert "title" in results[0]
+    assert "salary" in results[0]
+    assert "type" in results[0]
