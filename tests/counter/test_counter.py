df52203d761b66f4de626678cc9cec6746d8f7c8
Davi <df828316@gmail.com>
2022-10-20 19:24:13 -0300

    test: :test_tube: test for the method count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,12 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
+
+PATH = "src/jobs.csv"
+WORD_UPPERCASE = "PYTHON"
+WORD_LOWERCASE = "python"
+N = 1639
 
 
 def test_counter():
-    pass
+    word_count_uppercase = count_ocurrences(PATH, WORD_UPPERCASE)
+    word_count_lowercase = count_ocurrences(PATH, WORD_LOWERCASE)
+    assert word_count_uppercase == N == word_count_lowercase
