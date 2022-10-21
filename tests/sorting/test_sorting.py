b358a25ef519c6cc21d5bf89f030cbf85c43204e
Davi <df828316@gmail.com>
2022-10-21 16:42:54 -0300

    fix: :bug: lint error

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -3,7 +3,7 @@ from src.sorting import sort_by
 jobs = []
 
 
-def test_sort_by_criteria():  
+def test_sort_by_criteria():
     sort_by(jobs, "date_posted")
     sort_by(jobs, "max_salary")
     sort_by(jobs, "min_salary")
