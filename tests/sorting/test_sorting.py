2c35acc05afc8df0831afd40fbb79d8613027be6
Davi <df828316@gmail.com>
2022-10-21 16:39:11 -0300

    test: :test_tube: start the test for the method sort_by

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,11 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+jobs = []
 
 
-def test_sort_by_criteria():
-    pass
+def test_sort_by_criteria():  
+    sort_by(jobs, "date_posted")
+    sort_by(jobs, "max_salary")
+    sort_by(jobs, "min_salary")
+    # if sort_by(jobs, "invalid"):
+    #     XFAIL(Expected Fail)
